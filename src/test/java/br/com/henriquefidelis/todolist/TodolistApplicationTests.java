7bb088ad8db88e11a11b1f379e686c911a4aff18
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-10-02 00:22:56 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/henriquefidelis/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henriquefidelis.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
