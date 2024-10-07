d5befb439ec29c166f8ff2afa22ee0dd01ed85bd
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-10-07 15:23:59 -0300

    Inserindo métodos getters e setters

--- src/main/java/br/com/henriquefidelis/todolist/users/UserModel.java
+++ src/main/java/br/com/henriquefidelis/todolist/users/UserModel.java
@@ -2,8 +2,30 @@ package br.com.henriquefidelis.todolist.users;
 
 public class UserModel {
     
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    // Métodos getters e setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
 
 }
