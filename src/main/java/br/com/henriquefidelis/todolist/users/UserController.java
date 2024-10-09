1116ac5eff3ec5eb089896ed0bc52695bc90749a
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-10-09 14:19:58 -0300

    Utilizando ResponseEntity para retornos personalizados

--- src/main/java/br/com/henriquefidelis/todolist/users/UserController.java
+++ src/main/java/br/com/henriquefidelis/todolist/users/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -15,16 +17,15 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null) {
-            System.out.println("Usuário já existe");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         var useCreated = this.userRepository.save(userModel);
-        return useCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(useCreated);
     }
     
 }
