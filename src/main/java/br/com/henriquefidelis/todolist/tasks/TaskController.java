d03c5960ac49dd96d3ffd4ad78fe4a03b671639a
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-10-09 15:19:15 -0300

    Realizando o cadastro de tarefas

---
+++ src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
@@ -0,0 +1,21 @@
+package br.com.henriquefidelis.todolist.tasks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel) {
+        return this.taskRepository.save(taskModel);
+    }
+
+}
