0447931ad3f2f431faf030081f566f97e35e49b0
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-10-11 16:14:49 -0300

    Setando atributo na requisição

--- src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
+++ src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
@@ -1,9 +1,14 @@
 package br.com.henriquefidelis.todolist.tasks;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -14,8 +19,10 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
-        System.out.println("Chegou no controller");
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        System.out.println("Chegou no controller" + request.getAttribute("userId"));
+        var userId = request.getAttribute("userId");
+        taskModel.setUserId((UUID)userId);
         return this.taskRepository.save(taskModel);
     }
 
