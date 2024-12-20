e97931680919ed0181773c728df9d1a53cf0a096
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-12-20 13:01:45 -0300

    Validando a permissão de atualização de tarefas

--- src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
+++ src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
@@ -53,12 +53,23 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
 
-        Utils.copyNonNullProperties(taskModel, task);
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
 
-        return this.taskRepository.save(task);
+        var userId = request.getAttribute("userId");
+
+        if (!task.getUserId().equals(userId)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Usuário não tem autorização para alterar essa tarefa");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 
 }
