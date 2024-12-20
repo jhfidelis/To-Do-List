0eefa45f3c39469a6b890c482253f5cf012dc829
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-12-20 12:36:48 -0300

    Realizando a atualização parcial dos atributos da tarefa

--- src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
+++ src/main/java/br/com/henriquefidelis/todolist/tasks/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.henriquefidelis.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,10 +54,11 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var userId = request.getAttribute("userId");
-        taskModel.setUserId((UUID)userId);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 
 }
